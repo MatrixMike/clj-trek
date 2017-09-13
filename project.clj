de9f1bc84a1612b6a8172726b41cb3bff915465e
Mike J Hewitt <mikeh@electroteach.com>
2017-09-14 08:22:21 +1000

    whereis instaparse

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject pelicans "0.1.1-SNAPSHOT"
+(defproject clj-trek "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   
-  :main pelicans.core
+  :main trek.core
   :aot :all
   :omit-source true
   )
